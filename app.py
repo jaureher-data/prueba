75fa789dbf23d86555edcfb94603f613d380e334
jaureher-data <jaureher@gmail.com>
2026-05-20 01:55:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
+import os
+from pathlib import Path
+
+# Crear secrets.toml dinámicamente
+def crear_secrets_streamlit():
+
+    Path(".streamlit").mkdir(exist_ok=True)
+
+    secrets = f"""
+[auth]
+redirect_uri = "{os.environ["AUTH_REDIRECT_URI"]}"
+cookie_secret = "{os.environ["AUTH_COOKIE_SECRET"]}"
+
+[auth.google]
+client_id = "{os.environ["GOOGLE_CLIENT_ID"]}"
+client_secret = "{os.environ["GOOGLE_CLIENT_SECRET"]}"
+server_metadata_url = "https://accounts.google.com/.well-known/openid-configuration"
+"""
+
+    Path(".streamlit/secrets.toml").write_text(
+        secrets.strip(),
+        encoding="utf-8"
+    )
+
+crear_secrets_streamlit()
+
+# IMPORTANTE:
+# Streamlit se importa DESPUÉS
 import streamlit as st
 
-st.set_page_config(page_title="App con login Gmail")
+st.title("App protegida")
 
 if not st.user.is_logged_in:
-    st.title("Inicio de sesión")
-    st.write("Debes iniciar sesión con tu cuenta de Google.")
-    st.login("google")
+
+    if st.button("Login Google"):
+        st.login("google")
+
     st.stop()
 
-st.sidebar.write(f"Usuario: {st.user.email}")
-
-if st.sidebar.button("Cerrar sesión"):
-    st.logout()
-
-st.title("Aplicación protegida")
-st.success(f"Bienvenido, {st.user.name}")
-
-st.write("Contenido privado de la app.")
+st.success(f"Bienvenido {st.user.email}")
